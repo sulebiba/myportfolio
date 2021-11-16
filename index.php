4bac44c2a650730a2d22ab97023ba56e4c117aff
sulebiba <84573097+sulebiba@users.noreply.github.com>
2021-11-16 19:52:55 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,361 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="images/project/user.png" rel="icon">
+    <title>Suleiman Mohammed</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/unicons.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- MAIN STYLE -->
+    <link rel="stylesheet" href="css/tooplate-style.css">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="js/validation.js"></script>
+<!--
+
+Tooplate 2115 Marvel
+
+https://www.tooplate.com/view/2115-marvel
+
+-->
+  </head>
+  <body>
+
+    <!-- MENU -->
+    <nav class="navbar navbar-expand-sm navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="index.html"><i class='uil uil-user'></i>Suleiman</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a href="#about" class="nav-link"><span data-hover="About">About</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#project" class="nav-link"><span data-hover="Projects">Projects</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#resume" class="nav-link"><span data-hover="Resume">Resume</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#contact" class="nav-link"><span data-hover="Contact">Contact</span></a>
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav ml-lg-auto">
+                    <div class="ml-lg-4">
+                      <div class="color-mode d-lg-flex justify-content-center align-items-center">
+                        <i class="color-mode-icon"></i>
+                        Color mode
+                      </div>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- ABOUT -->
+    <section class="about full-screen d-lg-flex justify-content-center align-items-center" id="about">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-lg-7 col-md-12 col-12 d-flex align-items-center">
+                    <div class="about-text">
+                        <small class="small-text">Welcome to <span class="mobile-block">my portfolio website!</span></small>
+                        <h1 class="animated animated-text">
+                            <span class="mr-2">Hey folks, I am</span>
+                                <div class="animated-info">
+                                    <span class="animated-item">Suleiman</span>
+                                    <span class="animated-item">Website Dev</span>
+                                    <span class="animated-item">Ethical Hacker</span>
+                                    
+                                </div>
+                        </h1>
+
+                        <p>I am a Web developer with a wide experience when it comes to building Web Applications. You can check out some of my works below</p>
+                        
+                        <div class="custom-btn-group mt-4">
+                          <a href="MyCV.pdf" class="btn mr-lg-2 custom-btn"><i class='uil uil-file-alt'></i> Download Resume</a>
+                          <a href="#contact" class="btn custom-btn custom-btn-bg custom-btn-link">Get a free quote</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-5 col-md-12 col-12">
+                    <div class="about-image svg">
+                        <img src="images/undraw/CUSTOM-WEB-APPLICATION-DEVELOPMENT.svg" class="img-fluid" alt="svg image">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- PROJECTS -->
+    <section class="project py-5" id="project">
+        <div class="container">
+                
+                <div class="row">
+                  <div class="col-lg-11 text-center mx-auto col-12">
+
+                      <div class="col-lg-8 mx-auto">
+                        <h2>Some of the Websites I have built</h2>
+                      </div>
+
+                      <div class="owl-carousel owl-theme">
+                        <div class="item">
+                          <div class="project-info">
+                              <a href="https://nocv.ng" target="_blank"><img src="images/project/1.png" class="img-fluid" alt="project image"></a>
+                          </div>
+                        </div>
+
+                        <div class="item">
+                          <div class="project-info">
+                              <a href="https://myfamilypot.com" target="_blank"><img src="images/project/2.png" class="img-fluid" alt="project image"></a>
+                          </div>
+                        </div>
+
+                        <div class="item">
+                          <div class="project-info">
+                              <a href="https://stuckongo.ng" target="_blank"><img src="images/project/3.png" class="img-fluid" alt="project image"></a>
+                          </div>
+                        </div>
+
+                        <div class="item">
+                          <div class="project-info">
+                              <a href="https://nelsdaily.com" target="_blank"><img src="images/project/4.png" class="img-fluid" alt="project image"></a>
+                          </div>
+                        </div>
+
+                        <div class="item">
+                          <div class="project-info">
+                              <a href="https://aderayooyegbade.com" target="_blank"><img src="images/project/5.png" class="img-fluid" alt="project image"></a>
+                          </div>
+                        </div>
+                      </div>
+
+                  </div>
+                </div>
+        </div>
+    </section>
+
+    <!-- FEATURES -->
+    <section class="resume py-5 d-lg-flex justify-content-center align-items-center" id="resume">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-6 col-12">
+                  <h2 class="mb-4">Experiences</h2>
+
+                    <div class="timeline">
+                        <div class="timeline-wrapper">
+                             <div class="timeline-yr">
+                                  <span>2016</span>
+                             </div>
+                             <div class="timeline-info">
+                                  <h3><span>SwapSpace</span><small>We design and Development</small></h3>
+                                  <p>Worked on a personal income Tax enlightenment Website with everything I learnt here</p>
+                             </div>
+                        </div>
+
+                        <div class="timeline-wrapper">
+                            <div class="timeline-yr">
+                                <span>2019</span>
+                            </div>
+                            <div class="timeline-info">
+                                <h3><span>NYSC</span><small>IT department</small></h3>
+                                <p>During my Service Year, I Worked in the Principals Office at Methodist 
+                                   grammar School, bodija, Ojunrin, Ibadan and helped in solving any 
+                                   computer related problem mostly with the use of MS-EXCEL and MS WORD.</p>
+                            </div>
+                        </div>
+
+                       
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-12">
+                  <h2 class="mb-4 mobile-mt-2">Education</h2>
+
+                    <div class="timeline">
+                         <div class="timeline-wrapper">
+                             <div class="timeline-yr">
+                                  <span>2021</span>
+                             </div>
+                             <div class="timeline-info">
+                                  <h3><span>New Horizons</span><small>Networking</small></h3>
+                                  <p>CompTIA Networks
+                                  </p>
+                             </div>
+                        </div>
+                        <div class="timeline-wrapper">
+                             <div class="timeline-yr">
+                                  <span>2021</span>
+                             </div>
+                             <div class="timeline-info">
+                                  <h3><span>Google</span><small>Google Analytics</small></h3>
+                                  <p>Google Analytics for beginners
+                                  </p>
+                             </div>
+                        </div>
+                        <div class="timeline-wrapper">
+                             <div class="timeline-yr">
+                                  <span>2020</span>
+                             </div>
+                             <div class="timeline-info">
+                                  <h3><span>Google</span><small>Digital Marketing</small></h3>
+                                  <p>Google Digital Skills for Africa – The Fundamentals of Digital 
+                                     Marketing
+                                  </p>
+                             </div>
+                        </div>
+                        <div class="timeline-wrapper">
+                             <div class="timeline-yr">
+                                  <span>2019</span>
+                             </div>
+                             <div class="timeline-info">
+                                  <h3><span>New Horizons</span><small>Adv Web</small></h3>
+                                  <p>Advanced Web
+                                  </p>
+                             </div>
+                        </div>
+                        <div class="timeline-wrapper">
+                             <div class="timeline-yr">
+                                  <span>2018</span>
+                             </div>
+                             <div class="timeline-info">
+                                  <h3><span>New Horizons</span><small>Web Design</small></h3>
+                                  <p>Diploma Web Design Certificate (December 2018)
+                                  </p>
+                             </div>
+                        </div>
+                        <div class="timeline-wrapper">
+                             <div class="timeline-yr">
+                                  <span>2018</span>
+                             </div>
+                             <div class="timeline-info">
+                                  <h3><span>Babcock University</span><small>Computer Science</small></h3>
+                                  <p>BSc Computer Science (Second class upper division) 4.16 
+                                  CGPA</p>
+                             </div>
+                        </div>
+
+                        <div class="timeline-wrapper">
+                            <div class="timeline-yr">
+                                <span>2015</span>
+                            </div>
+                            <div class="timeline-info">
+                                <h3><span>NIIT</span><small>Oracle Database</small></h3>
+                                <p>Certificate NIIT - Oracle Database 11G</p>
+                            </div>
+                        </div>
+                        
+                       
+
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+    <!-- CONTACT -->
+    <section class="contact py-5" id="contact">
+      <div class="container">
+        <div class="row">
+          
+          <div class="col-lg-5 mr-lg-5 col-12">
+            
+
+            <div class="contact-info d-flex justify-content-between align-items-center py-4 px-lg-5">
+                <div class="contact-info-item">
+                  <h3 class="mb-3 text-white">Say hello</h3>
+                  <p><a href="mailto:mohammedsuleiman96@gmail.com">mohammedsuleiman96@gmail.com</a></p>
+                </div>
+
+                <ul class="social-links">
+                    <br><br><a href="https://github.com/sulebiba" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="25" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                        </svg></a>
+                </ul>
+            </div>
+          </div>
+
+          <div class="col-lg-6 col-12">
+            <div class="contact-form">
+              <h2 class="mb-4">Interested to work together? Let's talk</h2>
+
+              <form action="form.php" method="post" onsubmit='return validate()'>
+                <div class="row">
+                  <div class="col-lg-6 col-12">
+                    <input type="text" class="form-control" name="name" placeholder="First Name" id="name">
+                      <span id="error" class="error">Enter Your Firstname</span>
+                  </div>
+
+                  <div class="col-lg-6 col-12">
+                    <input type="email" class="form-control" name="email" placeholder="Email" id="email">
+                      <span id="error2" class="error">Enter Your Email Address</span>
+                  </div>
+
+                  <div class="col-12">
+                    <textarea name="message" rows="6" class="form-control" id="message" placeholder="Message"></textarea>
+                      <span id="error3" class="error">Enter a message</span>
+                  </div>
+                   <input type="hidden"name="date"value="<?php echo date('Y-m-d'); ?>"/>
+                  <div class="ml-lg-auto col-lg-5 col-12">
+                    <input type="submit" class="form-control submit-btn" value="Send Button" name="submit">
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <!-- FOOTER -->
+     <footer class="footer py-5">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-lg-12 col-12">                                
+                        <p class="copyright-text text-center">Copyright &copy; 2021 sulebiba . All rights reserved</p>
+                        </p>
+                    </div>
+                    
+               </div>
+          </div>
+     </footer>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/Headroom.js"></script>
+    <script src="js/jQuery.headroom.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="js/custom.js"></script>
+
+  </body>
+</html>
+      <?php
+if(isset($_GET['success'])){
+    echo"<script>message('Submission Successful!', 'Thank you very much!', 'success')</script>";
+}
+
+
+?>
